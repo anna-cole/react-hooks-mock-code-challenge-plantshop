d884b13f5b56a6b9800dd5457745cf492bba275f
Anna Raptidou Cole <acrrj123@gmail.com>
2023-10-18 13:07:34 -0500

    form to add new plant working

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,12 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import Header from "./Header";
 import PlantPage from "./PlantPage";
 
 function App() {
-  const [plants, setPlants] = useState([])
-
-  useEffect(() => {
-    fetch('http://localhost:6001/plants')
-    .then(r => r.json())
-    .then(plants => setPlants(plants))
-  }, [])
-
   return (
     <div className="app">
       <Header />
-      <PlantPage plants={plants}/>
+      <PlantPage />
     </div>
   );
 }
--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,12 +1,24 @@
-import React from "react";
+import React, { useEffect, useState }  from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
-function PlantPage({ plants }) {
+function PlantPage() {
+  const [plants, setPlants] = useState([])
+
+  useEffect(() => {
+    fetch('http://localhost:6001/plants')
+    .then(r => r.json())
+    .then(plants => setPlants(plants))
+  }, [])
+
+  function submitNewPlant(newPlant) {
+    setPlants([...plants, newPlant])
+  }
+
   return (
     <main>
-      <NewPlantForm />
+      <NewPlantForm submitNewPlant={submitNewPlant}/>
       <Search />
       <PlantList plants={plants}/>
     </main>
